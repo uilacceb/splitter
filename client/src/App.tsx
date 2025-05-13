76e07e9ec4bcfae9f239952f4002ef3ac3a66280
uilacceb <qu6qu6g@gmail.com>
2025-05-13 08:42:13 -0400

    set up router and tailwind for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import CreateEventPage from "./pages/CreateEventPage";
+import AddParticipantsPage from "./pages/AddParticipantsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import AddExpensePage from "./pages/AddExpensePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/create-event" element={<CreateEventPage />} />
+        <Route
+          path="/event/:eventId/add-participants"
+          element={<AddParticipantsPage />}
+        />
+        <Route
+          path="/event/:eventId/add-expense"
+          element={<AddExpensePage />}
+        />
+        <Route path="/event/:eventId/details" element={<EventDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/CreateEventPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateEventPage = () => {
+  return (
+    <div>CreateEventPage</div>
+  )
+}
+
+export default CreateEventPage
